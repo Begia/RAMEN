71ca8d29f16211b4a150e05dc4238068e2227cd0
Begia <matti.ruuskanen@gmail.com>
2016-07-19 01:36:26 +0300

    uc_to_OTU_table.R

---
+++ uc_to_OTU_table.R
@@ -0,0 +1,70 @@
+#!/usr/bin/Rscript
+#Matti Ruuskanen, Jul 2016
+
+ipak <- function(pkg) {
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+packages <- "optparse"
+ipak(packages)
+
+ncores <- detectCores()
+
+option_list <- list(
+  make_option(
+    c("-f", "--file"),
+    type = "character",
+    default = NULL,
+    help = "dataset file name",
+    metavar = "character"
+  ),
+  make_option(
+    c("-o", "--out"),
+    type = "character",
+    default = getwd(),
+    help = "output folder [default = %default]",
+    metavar = "character"
+  ),
+)
+
+
+opt_parser <- OptionParser(option_list = option_list)
+
+opt <- parse_args(opt_parser)
+
+
+if (is.null(opt$file)) {
+  print_help(opt_parser)
+  stop("At least one argument must be supplied (input file).", call. = FALSE)
+}
+
+setwd("C:\Users\Matti\Documents\uOttawa\Tehsis")
+
+df <- read.table("merA_sorted_uc99.uc")
+df <- df[,-c(6, 7)]
+cSizes <- df[df$V1 == "C",]
+df <- df[!df$V1 == "C",]
+df <- df[, c(1, 2, 7)]
+df <- df[order(df$V2),]
+colnames(df) <- c("rType", "OTU", "Sample")
+df$Sample <- gsub("(.*)_.*", "\\1", df$Sample)
+df$Dummy <- rep(1, nrow(df))
+df2 <- aggregate(Dummy ~ Sample * OTU, FUN = length, data = df)
+otuTable <- (xtabs(Dummy ~ OTU + Sample, data = df2))
+names(attributes(otuTable)$dimnames) <- NULL
+otuTable = as.data.frame.matrix(otuTable)
+
+otuTable$OTU_ID = paste("OTU", row.names(otuTable), sep = "_")
+
+otuTable = otuTable[, c(length(otuTable), c(1:length(otuTable) - 1))]
+colnames(otuTable)[1] <- "#OTU_ID"
+write.table(
+  otuTable,
+  file = "otu_table.txt",
+  quote = F,
+  sep = "\t",
+  row.names = F
+)
