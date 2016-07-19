c25e237ba091b976f5ae4ba932e5f5600279e440
Begia <matti.ruuskanen@gmail.com>
2016-07-19 17:29:43 -0400

    Update uc_to_OTU_table.R

--- uc_to_OTU_table.R
+++ uc_to_OTU_table.R
@@ -1,5 +1,12 @@
 #!/usr/bin/Rscript
-#Matti Ruuskanen & Julian Evans, Jul 2016
+#Matti Ruuskanen, Jul 2016
+#Makes an OTU table from a clustered *.uc file.
+#Possibility to remove OTUs from the OTU table with less than a specified number of reads.
+#If given a matching fasta file of cluster seeds, renames the sequences and removes
+#OTUs with less than the user specified minimum number of reads also from the fasta file.
+
+suppressPackageStartupMessages(library("optparse"))
+suppressPackageStartupMessages(library("Biostrings"))
 
 ipak <- function(pkg) {
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
@@ -8,15 +15,15 @@ ipak <- function(pkg) {
   sapply(pkg, require, character.only = TRUE)
 }
 
-packages <- "optparse"
+packages <- c("optparse", "Biostrings")
 ipak(packages)
 
 option_list <- list(
   make_option(
-    c("-f", "--file"),
+    c("-c", "--clusters"),
     type = "character",
     default = NULL,
-    help = "dataset file name",
+    help = "cluster file name",
     metavar = "character"
   ),
   make_option(
@@ -26,6 +33,20 @@ option_list <- list(
     help = "output folder [default = %default]",
     metavar = "character"
   ),
+  make_option(
+    c("-f", "--fasta"),
+    type = "character",
+    default = NULL,
+    help = "fasta file name containing cluster seeds",
+    metavar = "character"
+  ),
+  make_option(
+    c("-m", "--min"),
+    type = "integer",
+    default = 0,
+    help = "minimum number of reads in OTU to be included [default = disabled]",
+    metavar = "integer"
+  )
 )
 
 
@@ -34,23 +55,42 @@ opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
 
-if (is.null(opt$file)) {
+if (is.null(opt$clusters)) {
   print_help(opt_parser)
   stop("At least one argument must be supplied (input file).", call. = FALSE)
 }
 
 setwd(opt$out)
 
-df <- read.table(opt$file)
+df <- read.table(opt$clusters)
 df <- df[,-c(6, 7)]
+
 cSizes <- df[df$V1 == "C",]
-df <- df[!df$V1 == "C",]
+cSizes <- cSizes[, 2:3]
+cSizes[, 1] <- paste("OTU", cSizes[, 1], sep = "_")
+colnames(cSizes) <- c("#OTU_ID", "count")
+removedOTUs <- cSizes[cSizes$count <= opt$min,]
+cSizes <- cSizes[!cSizes$`#OTU_ID` %in% removedOTUs$`#OTU_ID`,]
+
+if (opt$min > 0) {
+  print(paste(
+    "Removed ",
+    nrow(cSizes),
+    " OTUs with size under the threshold of ",
+    opt$min,
+    sep = ""
+  ))
+  print("")
+}
+
+df <- df[!df$V1 == "C", ]
 df <- df[, c(1, 2, 7)]
-df <- df[order(df$V2),]
+df <- df[order(df$V2), ]
 colnames(df) <- c("rType", "OTU", "Sample")
 df$Sample <- gsub("(.*)_.*", "\\1", df$Sample)
 df$Dummy <- rep(1, nrow(df))
 df2 <- aggregate(Dummy ~ Sample * OTU, FUN = length, data = df)
+
 otuTable <- (xtabs(Dummy ~ OTU + Sample, data = df2))
 names(attributes(otuTable)$dimnames) <- NULL
 otuTable = as.data.frame.matrix(otuTable)
@@ -59,6 +99,30 @@ otuTable$OTU_ID = paste("OTU", row.names(otuTable), sep = "_")
 
 otuTable = otuTable[, c(length(otuTable), c(1:length(otuTable) - 1))]
 colnames(otuTable)[1] <- "#OTU_ID"
+
+otuTable <- otuTable[otuTable$`#OTU_ID` %in% cSizes$`#OTU_ID`, ]
+
+print(paste(
+  "A total of ",
+  colSums(cSizes[2]),
+  " reads in ",
+  nrow(cSizes),
+  " OTUs, with:",
+  sep = ""
+))
+print("")
+for (i in 2:length(colnames(otuTable))) {
+  print(paste(
+    colnames(otuTable)[i],
+    ": ",
+    colSums(otuTable[i]),
+    " reads in ",
+    length(which(otuTable[, i] != 0)),
+    " OTUs",
+    sep = ""
+  ))
+}
+
 write.table(
   otuTable,
   file = "otu_table.txt",
@@ -66,3 +130,16 @@ write.table(
   sep = "\t",
   row.names = F
 )
+
+if (!is.null(opt$fasta)) {
+  cSeeds <- readDNAStringSet(opt$fasta)
+  names(cSeeds) <-
+    paste("OTU", seq(1:length(names(cSeeds))), sep = "_")
+  cSeeds <- cSeeds[which(names(cSeeds) %in% otuTable$`#OTU_ID`)]
+  fileName <- sub("(.+)\\.f.*", "\\1", opt$fasta)
+  writeXStringSet(
+    cSeeds,
+    filepath = paste(fileName, "renamed.fasta", sep = "_"),
+    format = "fasta"
+  )
+}
